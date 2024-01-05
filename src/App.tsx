c5d06657354076fec41da0effe67be286c45ad0a
balamurugan16 <balamurugan.07@outlook.com>
2024-01-05 10:34:37 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Header } from "./components";
+
+function App() {
+	return (
+		<div className="container">
+			<Header />
+			<div className="container-view">Some activities should be here</div>
+		</div>
+	);
+}
+
+export default App;
